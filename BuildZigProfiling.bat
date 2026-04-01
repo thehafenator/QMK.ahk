7975af0d130071f0a221e32daf2bf43aaa0a687b
thehafenator <107648184+thehafenator@users.noreply.github.com>
2026-04-01 13:32:24 -0500

    Update BuildZigProfiling.bat for DLL name changeBuild your own Zig DLL or Interception to String
    For security/peace of mind/adjusting your own version of the DLL, feel free to use this bat file to compile. You'll need to change the location of your zig installation, name/location of the zig file to compile, and where you want it to go. Also change the target of compilation depending on your processor. 
    
    Also do The final part with the appropriate interception dll to copy to clipboard.

--- BuildZigProfiling.bat
+++ BuildZigProfiling.bat
@@ -4,12 +4,12 @@ if /i "%~1" NEQ "__stayopen__" (
   exit /b
 )
 
+
 set "ZIG=C:\Program Files\zig\zig.exe"
 set "DEST=C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib"
-set "SCRIPT=C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib\Macropad Compile.ahk"
 set "SRC=C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib\Libraries\QMKInterception\QMKCoreInterceptiondebugprofiling.zig"
 set "LIB_DIR=C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib\Libraries\QMKInterception"
-set "DLL=QMKCoreZigProfiling.dll"
+set "DLL=QMKCore.dll"
 
 if not exist "%ZIG%" ( echo ERROR: zig.exe not found & pause & exit /b 1 )
 if not exist "%SRC%" ( echo ERROR: source missing & pause & exit /b 1 )
@@ -37,13 +37,12 @@ if %ERRORLEVEL% NEQ 0 (
 echo Build OK - copying...
 copy /Y "%DLL%" "%DEST%\%DLL%"
 if %ERRORLEVEL% NEQ 0 ( echo COPY FAILED & pause & exit /b 1 )
-
 echo SUCCESS: %DLL% copied to %DEST%
 
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
 "Add-Type -AssemblyName System.Windows.Forms; ^
-$qmk = [Convert]::ToBase64String([IO.File]::ReadAllBytes('C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib\Libraries\QMKInterception\QMKCoreZigProfiling.dll')); ^
+$qmk = [Convert]::ToBase64String([IO.File]::ReadAllBytes('C:\Users\s262925\OneDrive\Documents\AutoHotkey\lib\Libraries\QMKInterception\QMKCore.dll')); ^
 [System.Windows.Forms.Clipboard]::SetText($qmk); ^
 Write-Host 'Copied to clipboard!'"
 pause
